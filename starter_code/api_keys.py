b7fab76ebf98e7818d09d2423fc7d268b5ee17fb
Chris-Barrera <133803504+Chris-Barrera@users.noreply.github.com>
2023-07-28 00:57:16 -0400

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "760e5876b0e82bf79e4127f65eee6822"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "1d9f20146b684367b70f321fe2d6923e"
